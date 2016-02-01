ea45721ddf3bf22ee7679d2bb8e279840ced3cf3
stcalica <stcalica@gmail.com>
2016-01-31 19:12:46 -0800

    added files

---
+++ gds2cvs.py
@@ -0,0 +1,21 @@
+#!/bin/py
+
+import os, sys
+import csv 
+from gfsCAD import *
+
+
+
+def load_file():
+	pass
+
+def convert_to_cvs():
+	pass
+
+def main():
+	load_file()
+	convert_to_csv() 
+	return
+
+
+main() 
